009e60da2cd72d9e4f989476fad8986dd0da4cff
konstantinchurilov <konstantinchurilov22@gmail.com>
2017-10-31 16:14:56 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
 
+/**
+*Class Calculate решает задачи части 001 урок1.
+*@author kchurilov
+*@since 31.10.2017
+*/
 public class Calculate{
+	/**
+	* Точка входа.
+	*@param args - входные аргументы.
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello world");
 	}
